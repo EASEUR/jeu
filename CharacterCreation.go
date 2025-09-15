0b3e22f849261658813eef3235c6aa887af1c3e7
Enzo Del-Rey <enzo.delrey@ynov.com>
2025-09-15 16:43:52 +0200

    ff

--- CharacterCreation.go
+++ CharacterCreation.go
@@ -53,16 +53,3 @@ func CreateCharacter() Character {
 
 	return char
 }
-
-// Nouvelle fonction pour initialiser un personnage avec des valeurs précises
-func InitCharacter(name string, class string, level int, hp int, inventory []string) Character {
-	return Character{
-		Name:      name,
-		Class:     class,
-		Level:     level,
-		HP:        hp,
-		Power:     50,
-		Gold:      20,
-		Inventory: inventory,
-	}
-}
