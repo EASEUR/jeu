038fa4afd0aa10a24bc1cc1b9046955d48d6560d
voscar <oscar.vivien@ynov.com>
2025-09-16 09:16:04 +0200

    menu

--- Oscar.go
+++ Oscar.go
@@ -54,14 +54,40 @@ func menu() {
 			fmt.Scanln(&sousmenu) // choix sous menu
 			switch sousmenu {
 			case 1:
-				// continuez
+				logoontop()
+				fmt.Println("en vrai relou parce que c'est pas fini, reviens dans le menu")
+				fmt.Println("1- Oui")
+				fmt.Println("2- Oui")
+				fmt.Println("\n\n\n\n\n\n")
+				fmt.Scanln(&sousmenu)
+				break // sortir
 			case 2:
 				break // sortir
 			}
-		case 2:
-			// ouverture des parametres
+		case 2: // ouverture des parametres
+			logoontop()
+			fmt.Println("Paramètres")
+			fmt.Println("1- Langues")
+			fmt.Println("2- Quitter")
+			fmt.Println("\n\n\n\n\n\n")
+			fmt.Scanln(&sousmenu)
+			// changer la langue mais y'en a qu'une
+			logoontop()
+			fmt.Println("Choisir la langue :")
+			fmt.Println("1- Français")
+			fmt.Println("2- Quitter")
+			fmt.Println("\n\n\n\n\n\n")
+			fmt.Scanln(&sousmenu)
+			break
 		case 3:
-			// ouvrir l'inventaire
+			logoontop() // ouvrir l'inventaire
+			fmt.Println("Pourquoi tu veux voir l'inventaire, t'as même pas commencer le jeu, 0/20")
+			fmt.Println("Et pour info y'a rien dans l'inventaire gros nullos")
+			fmt.Println("1- Quitter")
+			fmt.Println("\n\n\n\n\n\n")
+			fmt.Scanln(&sousmenu)
+			break
+
 		case 4:
 			return // fermer prg
 		}
