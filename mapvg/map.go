f731b4b4ba02d30ab6c33506a451907869d27052
voscar <oscar.vivien@ynov.com>
2025-09-17 09:49:08 +0200

    caca

--- mapvg/map.go
+++ mapvg/map.go
@@ -3,12 +3,59 @@ package mapvg
 import (
 	"fmt"
 	"jeu/logo"
+	"math/rand"
+	"time"
 )
+
+func eprv1() bool {
+	var r1 int
+	logo.Logoontop()
+	fmt.Println("\033[1mVous êtes arrivé à la première épreuve\033[0m")
+	fmt.Println("\033[1mQuelle est la réponse à la vie ?\033[0m")
+	fmt.Println("\n\n\n\n\n\n\n\n")
+	fmt.Scanln(&r1)
+	for r1 != 42 {
+		logo.Logoontop()
+		fmt.Println("\033[1mVous vous êtes trompé, veuillez réessayez\033[0m")
+		fmt.Println("\033[1mQuelle est la réponse à la vie ?\033[0m")
+		fmt.Println("\033[3mPetit indice :(-80538738812075974)^3 + (80435758145817515)^3 + (12602123297335631)^3\033[0m")
+		fmt.Println("\n\n\n\n\n\n\n")
+		fmt.Scanln(&r1)
+	}
+	return true
+}
+
+func eprv2() bool {
+	var r2 int
+	rand.Seed(time.Now().UnixNano())
+	n1 := rand.Intn(1000) + 1
+	n2 := rand.Intn(1000) + 1
+	n3 := rand.Intn(1000) + 1
+
+	resultat := (n1 * n2) / n3
+
+	logo.Logoontop()
+	fmt.Println("\033[1mVous êtes arrivé à la première épreuve\033[0m")
+	fmt.Println("Voici le petit calcul à résoudre :")
+	fmt.Printf("(%d * %d) / %d = ? ", n1, n2, n3)
+	fmt.Println("\n\n\n\n\n\n\n")
+	fmt.Scanln(&r2)
+	for r2 != resultat {
+		logo.Logoontop()
+		fmt.Println("\033[1mVous vous êtes trompé, veuillez réessayez\033[0m")
+		fmt.Printf("(%d * %d) / %d = ? ", n1, n2, n3)
+		fmt.Println("\033[3m(Ne mettez pas les nombres après la virgule)\033[0m")
+		fmt.Println("\n\n\n\n\n\n\n")
+		fmt.Scanln(&r2)
+	}
+	return true
+}
 
 func piece1() bool {
 	var buff string
+	eprv1()
 	logo.Logoontop()
-	fmt.Println("Vous avez trouver la 1ere piece gg wp")
+	fmt.Println("\033[1mBravo, vous avez obtenu la 1ere pièce\033[0m")
 	fmt.Println("1- Continuez")
 	fmt.Println("\n\n\n\n\n\n\n\n")
 	fmt.Scanln(&buff)
@@ -16,8 +63,9 @@ func piece1() bool {
 }
 func piece2() bool {
 	var buff string
+	eprv2()
 	logo.Logoontop()
-	fmt.Println("Vous avez trouver la 2eme piece gg wp")
+	fmt.Println("\033[1mBravo, vous avez obtenu la 2eme pièce\033[0m")
 	fmt.Println("1- Continuez")
 	fmt.Println("\n\n\n\n\n\n\n\n")
 	fmt.Scanln(&buff)
@@ -26,7 +74,7 @@ func piece2() bool {
 func piece3() bool {
 	var buff string
 	logo.Logoontop()
-	fmt.Println("Vous avez trouver la 3eme piece gg wp")
+	fmt.Println("Vous avez trouvé la 3eme piece gg wp")
 	fmt.Println("1- Continuez")
 	fmt.Println("\n\n\n\n\n\n\n\n")
 	fmt.Scanln(&buff)
